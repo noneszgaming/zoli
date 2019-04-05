01d1c6ba5d524dbd137ae169025591a935886836
noneszgaming <48924782+noneszgaming@users.noreply.github.com>
2019-04-05 08:35:05 +0200

    Add files via upload

---
+++ GameHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("GameHandler elindult!");
+        GameObject snakeHeadGameObject = new GameObject();
+        SpriteRenderer snakeSpriteRender = snakeHeadGameObject.AddComponent<SpriteRenderer>();
+        snakeSpriteRender.sprite = GameAssets.i.snakeHeadSprite;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2Int gridPosition;
+    private void Awake()
+    {
+        gridPosition = new Vector2Int(10, 10); 
+    }
+    private void Update()
+    {
+        transform.position = new Vector3(gridPosition.x, gridPosition.y);
+    }
+    
+}
